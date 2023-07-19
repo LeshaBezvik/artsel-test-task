fb05cca03356f93f262aa08089071c754e820d8f
LeshaBezvik <38912842+LeshaBezvik@users.noreply.github.com>
2023-07-19 19:36:58 +0300

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,137 @@
+const swiper = new Swiper('.swiper', {
+
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        },
+
+        390: {
+            slidesPerView: 1,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        },
+
+        768: {
+            slidesPerView: 2,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        },
+
+        992: {
+            slidesPerView: 2,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        },
+    }
+
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+    const submitBtn = document.getElementById("submitBtn");
+    const errorMessages = document.getElementById("emailError");
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const name = document.getElementById("name").value.trim();
+      const email = document.getElementById("email").value.trim();
+      const message = document.getElementById("message").value.trim();
+      const errorMessages = document.getElementById("emailError");
+      let valid = true;
+      errorMessages.innerHTML = "";
+  
+      if (name === "") {
+        errorMessages.innerHTML += "Enter your name.";
+        valid = false;
+      } else {
+        document.getElementById("name").classList.add("accept");
+      }
+  
+      emailError.textContent = "";
+
+      if (email === "") {
+        errorMessages.innerHTML += "Please complete this email field.";
+        valid = false;
+      } else if (!validateEmail(email)) {
+        errorMessages.innerHTML = "This email does not seem to look right.";
+        const emailInput = document.getElementById("email");
+        const emailLabel = document.getElementById("email-label");
+        emailInput.classList.add("invalid");
+        emailLabel.classList.add("invalid");
+        valid = false;
+      }
+
+      if (message === "") {
+        errorMessages.innerHTML += "Enter your message.";
+        valid = false;
+      } else {
+          document.getElementById("message").classList.add("accept");
+      }
+  
+      if (valid) {
+        form.submit();
+      }
+    });
+  
+    function validateEmail(email) {
+      const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      return emailPattern.test(email);
+    }
+  
+    form.addEventListener("input", function () {
+      const name = document.getElementById("name").value.trim();
+      const email = document.getElementById("email").value.trim();
+      const message = document.getElementById("message").value.trim();
+
+
+      if (name !== "" && email !== "" && message !== "") {
+        submitBtn.disabled = false;
+      } else {
+        submitBtn.disabled = true;
+      }
+    });
+  });
+  
